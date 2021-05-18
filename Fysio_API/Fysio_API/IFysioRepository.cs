bd62d251bbe991ab6318fb62459ff793682b018c
Max van Oord <max.van.oord@gmail.com>
2021-05-18 13:03:56 +0200

    api update

--- Fysio_API/Fysio_API/IFysioRepository.cs
+++ Fysio_API/Fysio_API/IFysioRepository.cs
@@ -1,22 +1,21 @@
-﻿//using Fysio_API.Dto;
-//using Fysio_API.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
+﻿using Fysio_API.Auth;
+using Fysio_API.Dto;
+using Fysio_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-//namespace Fysio_API
-//{
-//    public interface IFysioRepository
-//    {
-//        void AddClient(Client client);
-//        Client GetClient(int clientId);
-//        IEnumerable<Client> GetClientsTherapist(int therapistId);
-//        void AddTherapist(Therapist therapist);
-//        Therapist GetTherapist(int therapistId);
-//        void AttachClientToTherapist(int clientId, int therapistId);
-//        void AssignExerciseToClient(ClientExercise clientExercise);
-//        Exercise GetExercise(int exerciseId);
-//    }
-//}
+namespace Fysio_API
+{
+    public interface IFysioRepository
+    {
+        ApplicationUser GetClient(string clientId);
+        IEnumerable<ApplicationUser> GetClientsTherapist(string therapistId);
+        ApplicationUser GetTherapist(string therapistId);
+        void AttachClientToTherapist(int clientId, int therapistId);
+        void AssignExerciseToClient(ClientExercise clientExercise);
+        Exercise GetExercise(int exerciseId);
+    }
+}
